4eb844e8c826faef4790e8d897ff6577d746048d
guscarrillo <115580058+guscarrillo@users.noreply.github.com>
2023-06-09 16:50:38 -0600

    added project files

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const socektIO = require('socket.io');
+const connectSocketServer = require('./config/socketConnection');
+//const {authMiddleware}  = require('./utils/auth);
+require('dotenv').config();
+
+const db = require('./config/connection')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',true);
+
+const server = http.createServer(app);
+connectSocketServer(server);
+
+app.use(express.urlencoded({extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.get('/authorLogin', (req, res) => {
+  res.sendFile(path.join(__dirname, ''))
+})
+
+db.once('open', () => {
+  server.listen(PORT, () => {
+    console.log('Server listening...');
+  })
+})
