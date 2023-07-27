6360c9ce171450d29ff52b33b5ea559c4ef1ec52
Gustavo Carrillo (He/Him) <115580058+0zrk23@users.noreply.github.com>
2023-07-27 14:32:23 -0600

    Revert "Develop"

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,24 +1,6 @@
-import { useMutation } from '@apollo/client'
 import React from 'react'
-import { UPDATE_BOOK } from '../../utils/mutations'
 
 function Home() {
-  const [updateBook] = useMutation(UPDATE_BOOK);
-  
-  const handleUpdate = async (event) => {
-    event.preventDefault();
-    try{
-      const {data} = await updateBook({
-        variables:{
-          _id: "64bff2040b8dacda123bbcbc",
-          title: `num: ${Math.random()}`
-        }
-      })
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
   return (
     <div>
       Home
